b95878d8d5dd03c74b1bb5a66d0c5208317cae1b
Saisha <saishapayal@gmail.com>
2025-11-11 08:37:11 -0500

    11/11 Review Practice Set 2

---
+++ Lesson2-Collections_Essentials/Part4-ReviewSets/set2.py
@@ -0,0 +1,19 @@
+# Question 1: Code Tracing
+# 1300, 1500, 2700, 1500
+# Output: 4
+# Output: 7000
+
+# Question 2: Code Tracing
+# Output: 0x9F1aB3c...
+
+# Question 3: Code Writing
+def portfolio_value(holdings, prices):
+    sum = 0
+    for key, value in holdings.items():
+        multiplied = value * prices[key]
+        sum += multiplied
+    return round(sum, 2)
+            
+holdings = {"BTC": 0.5, "ETH": 8.2, "SOL": 50}
+prices = {"BTC": 62400, "ETH": 2480, "SOL": 142}
+print(portfolio_value(holdings, prices))
